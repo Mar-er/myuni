9a68ca412487a13ba3df846c6ca28327eb465910
Mar-er <243958407@qq.com>
2020-10-26 14:06:16 +0800

    feat: init project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout>
+      <Head>…</Head>
+      <div>fdfdfd</div>
+    </Layout>
+  )
+}
